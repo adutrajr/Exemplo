137c2c0c616f6f8510805d6544a29fb9b2b7cec3
Adriano Junior <adutrajr@gmail.com>
2015-04-23 12:22:24 -0400

    Initial commit

---
+++ app/src/main/java/br/com/adrianodutra/exemplo/JanelaComBotao.java
@@ -0,0 +1,18 @@
+package br.com.adrianodutra.exemplo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by junior on 22/04/15.
+ */
+public class JanelaComBotao extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
